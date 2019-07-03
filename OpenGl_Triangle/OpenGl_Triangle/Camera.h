8078dfd4df13fae4f0dbf1450364b33bd5e03229
clonekim <>
2019-07-03 17:21:04 +0900

    새로운 카메라 클래스 생성

---
+++ OpenGl_Triangle/OpenGl_Triangle/Camera.h
@@ -0,0 +1,21 @@
+//
+//  Camera.h
+//  OpenGl_Triangle
+//
+//  Created by clonekim on 03/07/2019.
+//  Copyright © 2019 clonekim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <GLKit/GLKMath.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Camera : NSObject
+{
+    
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OpenGl_Triangle/OpenGl_Triangle/Camera.m
@@ -0,0 +1,13 @@
+//
+//  Camera.m
+//  OpenGl_Triangle
+//
+//  Created by clonekim on 03/07/2019.
+//  Copyright © 2019 clonekim. All rights reserved.
+//
+
+#import "Camera.h"
+
+@implementation Camera
+
+@end
